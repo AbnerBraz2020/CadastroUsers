9a713351f8c5acafc9d17f1da8c2eb376896a3fa
Abner Gomes <abnerbraz2020@gmail.com>
2025-04-09 11:16:37 -0300

    Mudança de Estilo

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -25,17 +25,17 @@ function home() {
       <h1>Cadastro de Usuário</h1>
       <input type="text" name="name" id="" placeholder="Entre com o seu nome"/>
       <input type="text" name="email" id="" placeholder="Entre com o seu email"/>
-      <input type="number" name="age" id="" placeholder="Entre com a sua idade"/>
-      <button type="submit">Cadastrar</button>
+      <input type="number" name="age" id="" placeholder="Entre com a sua age"/>
+      <button type="button" className='button'>Cadastrar</button>
     </form>
 
   {users.map((user) => (
     
     <div key={user.id} className='card'>
       <div>
-        <p>Nome: <span>[user.name]</span></p>
-        <p>Email:<span>[user.email]</span></p>
-        <p>Idade:<span>[user.age]</span></p>
+        <p>Nome: <span>{user.name}</span></p>
+        <p>Email: <span>{user.email}</span></p>
+        <p>Idade: <span>{user.age}</span></p>
       </div>
 
       <button>
