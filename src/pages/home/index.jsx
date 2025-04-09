e0d5214d41bdbaae2899e9886344426d2954575f
Abner Gomes <abnerbraz2020@gmail.com>
2025-04-09 10:59:50 -0300

    Mudança de Estilo

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,27 +1,51 @@
 import './style.css'
+import Trash from '../../assets/react.svg'
 
 function home() {
 
+  const users = [
+    {
+      id:'askdkjnfasd56554',
+      name: 'Edir',
+      email:'edir@gmail.com',
+      age:26
+    },
+
+    {
+      id:'fgkyejyeigfs48945',
+      name: 'João Lucas',
+      email:'joao@gmail.com',
+      age:23
+    }
+  ]
+
   return (
-  
   <div>
-    <form action="">
+    <form action="container">
       <h1>Cadastro de Usuário</h1>
       <input type="text" name="name" id="" placeholder="Entre com o seu nome"/>
       <input type="text" name="email" id="" placeholder="Entre com o seu email"/>
       <input type="number" name="age" id="" placeholder="Entre com a sua idade"/>
       <button type="submit">Cadastrar</button>
     </form>
-    <div>
+
+  {users.map((user) => (
+    
+    <div key={user.id} className='card'>
       <div>
-        <p>Nome</p>
-        <p>Email</p>
-        <p>Idade</p>
+        <p>Nome: <span>[user.name]</span></p>
+        <p>Email:<span>[user.email]</span></p>
+        <p>Idade:<span>[user.age]</span></p>
       </div>
-      <button type="button">Trash</button>
-    </div>
+
+      <button>
+        <img src={Trash} alt="Trash"/>
+      </button>
+    
+      </div>
+
+    ))}
   </div>
-      
 
   )
 }
